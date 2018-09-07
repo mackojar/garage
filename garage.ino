20316dd6922c8ff37af47f69aad6c56bea0959b7
Jarkos <mackoj@tlen.pl>
2018-09-07 23:44:56 +0200

    status blink & wifi restarts

--- garage.ino
+++ garage.ino
@@ -8,66 +8,94 @@
 #define WIFI_RETRY_DELAY 500
 #define MAX_WIFI_INIT_RETRY 50
 
+#define STATUS_LED_TIMEOUT 5000
+#define STATUS_LED_BLINK_TIME 150
+
+unsigned long lastStatusBlinkTimeOn;
+unsigned long lastStatusBlinkTimeOff;
+
 ESP8266WebServer server(HTTP_REST_PORT);
 
-int getBlinkState( int num) {
-  if( num % 2 == 0) {
-    return LOW;
-  } else {
-    return HIGH;
-  }
-}
-
-int init_wifi() {
-  int retries = 0;
-
-  Serial.print("Connecting to WiFi AP...");
-  digitalWrite( LED_BUILTIN, getBlinkState( retries));
-  
+int initWifi() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWD);
-  // check the status of WiFi connection to be WL_CONNECTED
+}
+
+void initStatusLED() {
+  lastStatusBlinkTimeOn = millis() + STATUS_LED_TIMEOUT;
+  lastStatusBlinkTimeOff = lastStatusBlinkTimeOn + STATUS_LED_BLINK_TIME;
+}
+
+void initRestServer() {
+  server.begin();
+  Serial.println("HTTP REST Server Started");
+}
+
+int getWIFIBlinkState( int num) {
+  return num & 0x01;  // LOW and HIGH...
+}
+
+void connectWIFI() {
+  WiFi.disconnect();
+  initWifi();
+  Serial.println();
+  Serial.print("Connecting to WiFi AP...");
+  unsigned int retries = 0;  // force first getBlinkState to return LOW -> inverted (LOW->LED is on)
+  digitalWrite( LED_BUILTIN, getWIFIBlinkState( retries));
   while ((WiFi.status() != WL_CONNECTED) /*&& (retries < MAX_WIFI_INIT_RETRY) */) {
     retries++;
     delay(WIFI_RETRY_DELAY);
-    digitalWrite( LED_BUILTIN, getBlinkState( retries));
+    digitalWrite( LED_BUILTIN, getWIFIBlinkState( retries));
     Serial.print(".");
   }
+
+  Serial.println();
+  Serial.print("Connetted to ");
+  Serial.print(WIFI_SSID);
+  Serial.print(", IP: ");
+  Serial.println(WiFi.localIP());
+
   digitalWrite( LED_BUILTIN, HIGH);
-  return WiFi.status(); // return the WiFi connection status
+}
+
+void checkWIFI() {
+  if( WiFi.status() != WL_CONNECTED) {
+    server.close();
+    connectWIFI();
+    initStatusLED();
+    initRestServer();
+  } else {
+    blinkStatusLED();
+  }
+}
+
+void blinkStatusLED() {
+  unsigned long currentMillis = millis();
+  if( (long)(currentMillis - lastStatusBlinkTimeOn) >= STATUS_LED_TIMEOUT) {
+    digitalWrite( LED_BUILTIN, LOW);
+    lastStatusBlinkTimeOn += STATUS_LED_TIMEOUT;
+  } else if( (long)(currentMillis - lastStatusBlinkTimeOff) >= STATUS_LED_TIMEOUT) {
+    digitalWrite( LED_BUILTIN, HIGH);
+    lastStatusBlinkTimeOff += STATUS_LED_TIMEOUT;
+  }
 }
 
 void setup(void) {
-    Serial.begin(115200);
-    bool mounted = SPIFFS.begin();
-    if( mounted) {
-      Serial.println("SPIFFS mounted.");
-    } else {
-      Serial.println("Error mounting SPIFFS");
-    }
+  Serial.begin(115200);
+  bool mounted = SPIFFS.begin();
+  if( mounted) {
+    Serial.println("SPIFFS mounted.");
+  } else {
+    Serial.println("Error mounting SPIFFS");
+  }
 
-    pinMode( PIN_GARAGE_DOOR, OUTPUT);
-    pinMode( LED_BUILTIN, OUTPUT);
-    
-    if (init_wifi() == WL_CONNECTED) {
-        Serial.println();
-        Serial.print("Connetted to ");
-        Serial.print(WIFI_SSID);
-        Serial.print(", IP: ");
-        Serial.println(WiFi.localIP());
-    }
-    else {
-        Serial.print("Error connecting to: ");
-        Serial.println(WIFI_SSID);
-    }
+  pinMode( PIN_GARAGE_DOOR, OUTPUT);
+  pinMode( LED_BUILTIN, OUTPUT);    
 
-    config_rest_server_routing();
-
-    server.begin();
-    Serial.println("HTTP REST Server Started");
+  config_rest_server_routing();
 }
 
 void loop(void) {
-    server.handleClient();
+  checkWIFI();
+  server.handleClient();
 }
-
