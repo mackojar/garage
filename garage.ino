ec9a5c36347ed735f53a8f76bfd266662e00e92b
Jarkos <mackoj@tlen.pl>
2018-09-28 23:55:16 +0200

    blinking status + favicon

--- garage.ino
+++ garage.ino
@@ -8,7 +8,33 @@
 #define WIFI_RETRY_DELAY 500
 #define MAX_WIFI_INIT_RETRY 50
 
+#define STATUS_LED_TIMEOUT 5000
+#define STATUS_LED_BLINK_TIME 150
+
+unsigned long lastStatusBlinkTimeOn;
+unsigned long lastStatusBlinkTimeOff;
+
 ESP8266WebServer server(HTTP_REST_PORT);
+
+IPAddress staticIP(192,168,1,22);
+IPAddress gateway(192,168,1,1);
+IPAddress subnet(255,255,255,0);
+
+void initStatusLED() {
+  lastStatusBlinkTimeOn = millis() + STATUS_LED_TIMEOUT;
+  lastStatusBlinkTimeOff = lastStatusBlinkTimeOn + STATUS_LED_BLINK_TIME;
+}
+
+void blinkStatusLED() {
+  unsigned long currentMillis = millis();
+  if( (long)(currentMillis - lastStatusBlinkTimeOn) >= STATUS_LED_TIMEOUT) {
+    digitalWrite( LED_BUILTIN, LOW);
+    lastStatusBlinkTimeOn += STATUS_LED_TIMEOUT;
+  } else if( (long)(currentMillis - lastStatusBlinkTimeOff) >= STATUS_LED_TIMEOUT) {
+    digitalWrite( LED_BUILTIN, HIGH);
+    lastStatusBlinkTimeOff += STATUS_LED_TIMEOUT;
+  }
+}
 
 int getBlinkState( int num) {
   if( num % 2 == 0) {
@@ -26,7 +52,8 @@ int init_wifi() {
   
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWD);
-  // check the status of WiFi connection to be WL_CONNECTED
+  WiFi.config(staticIP, gateway, subnet);
+  
   while ((WiFi.status() != WL_CONNECTED) /*&& (retries < MAX_WIFI_INIT_RETRY) */) {
     retries++;
     delay(WIFI_RETRY_DELAY);
@@ -55,8 +82,7 @@ void setup(void) {
         Serial.print(WIFI_SSID);
         Serial.print(", IP: ");
         Serial.println(WiFi.localIP());
-    }
-    else {
+    } else {
         Serial.print("Error connecting to: ");
         Serial.println(WIFI_SSID);
     }
@@ -65,9 +91,12 @@ void setup(void) {
 
     server.begin();
     Serial.println("HTTP REST Server Started");
+
+    initStatusLED();
 }
 
 void loop(void) {
     server.handleClient();
+    blinkStatusLED();
 }
 
